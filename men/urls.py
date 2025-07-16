b74797c318b557333d2a608550573343f60bc75b
aleksander <lunin_aleksander833@mail.ru>
2025-07-16 18:13:20 +0300

    Add Files

---
+++ men/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+from .views import index, about, addpage, contact, login, show_post, show_category
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
